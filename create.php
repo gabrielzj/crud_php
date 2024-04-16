78d1a258aa9d36637ee24ebfd349002a3bd808a9
gabrielzj <gabrielzuinj@gmail.com>
2024-04-16 00:03:31 -0300

    add: empty input validation

--- create.php
+++ create.php
@@ -1,34 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style/form.css">
 </head>
 <body>
     <form method="POST">
         <div class="mb-3">
-            <label for="exampleInputTipo" class="form-label">Tipo</label>
-            <input type="text" class="form-control" id="InputTipo" name="tipo">
+            <label for="inputTipo" class="form-label">Tipo</label>
+            <input type="text" class="form-control" id="inputTipo" name="createtipo">
+            <div id="tipotext" class="form-text"></div>
         </div>
         <div class="mb-3">
-            <label for="exampleInputNome" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="InputNome" name="nome">
+            <label for="inputNome" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="inputNome" name="createname">
         </div>
-        <button type="submit" class="btn btn-primary">Enviar</button>
+        <button type="submit" class="btn btn-primary" name="submit_btn">Enviar</button>
     </form>
     <?php
-        require("dbconfig.php");
         //se o metodo post esta definido, ou seja, retorna true
-        if(isset($_POST['tipo'])){
-            //insere dados na tabela id eh null pois eh auto-increment
-            $sql = $conn ->prepare("INSERT INTO produto VALUES (null,?,?)");
-            $sql -> execute(array($_POST['tipo'],$_POST['nome']));
-            echo '<h2>Produto criado com sucesso</h2>';
-            echo "<script>alert('Produto cadastrado!')</script>";
-            header("Location: index.php");
+        if(isset($_POST['submit_btn'])){
+            $tipo = $_POST['createtipo'];
+            $nome = $_POST['createname'];
+            if(empty($tipo) || empty($nome)) {
+                echo 'Todos os campos devem estar preenchidos';
+            } else {
+                $query = "SELECT COUNT(*) FROM produto WHERE prod_nome = '$nome' ";
+                //insere dados na tabela id eh null pois eh auto-increment
+                $sql = $conn -> prepare("INSERT INTO produto VALUES (null,?,?)");
+                $sql -> execute(array($tipo,$nome));
+                echo "<script> alert('Produto cadastrado!') </script>";
+                echo "<scrpt>location.href = '?page=index'; </script>";
+            }
         }
     ?>
 </body>
