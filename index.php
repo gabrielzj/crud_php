17709e79860854b0628e02b4757f56a2700524c1
gabrielzj <gabrielzuinj@gmail.com>
2024-04-16 17:57:28 -0300

    add: css file link

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Produtos</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style/form.css">
     </head>
     <body>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -32,7 +33,7 @@
             </div>
         </nav>
         <?php
-            include('dbconfig.php');
+            include ('dbconfig.php');
                 //faz include do arquivo de acordo com o request da página
              switch (@$_REQUEST['page']) {
                 case 'create':
