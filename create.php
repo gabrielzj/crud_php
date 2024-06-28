fecf2c57533abda9ae744dd5fe689c46e220ee4d
gabrielzj <gabrielzuinj@gmail.com>
2024-06-28 16:48:15 -0300

    update: code refactoring

--- create.php
+++ create.php
@@ -1,35 +1,52 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
 </head>
+
 <body>
+    <h2>Cadastrar Produto</h2>
     <form action="?page=create" method="POST">
         <div class="mb-3">
             <label for="inputTipo" class="form-label">Tipo</label>
-            <input type="text" class="form-control" id="inputTipo" name="createtipo">
+            <input type="text" class="form-control border-primary" id="inputTipo" name="createtipo">
             <div id="tipotext" class="form-text"></div>
         </div>
         <div class="mb-3">
             <label for="inputNome" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="inputNome" name="createname">
+            <input type="text" class="form-control border-primary" id="inputNome" name="createname">
         </div>
         <button type="submit" class="btn btn-primary" name="submit_btn">Enviar</button>
     </form>
     <?php
-        //se o metodo post esta definido, ou seja, retorna true
-        if(isset($_REQUEST['submit_btn'])){
-            $tipo = strtolower($_REQUEST['createtipo']);
-            $nome = strtolower($_REQUEST['createname']);
-            if(empty($tipo) || empty($nome)) {
-                echo "<script> alert('Existem campos vazios!') </script>";
-            } else {
-                //insere dados na tabela id eh null pois eh auto-increment
-                $sql = $conn -> prepare("INSERT INTO produto VALUES (null,?,?)");
-                $sql -> execute(array($tipo,$nome));
-                echo "<script> alert('Produto cadastrado!') </script>";
-                header("Location: index.php?page=index");
+
+    function createProduct($conn, $tipo, $nome)
+    {
+        $query = "INSERT INTO produto VALUES (null,?,?)";
+        $sql = $conn->prepare($query);
+        $sql->execute(array($tipo, $nome));
+        echo "<script> 
+                alert('Produto cadastrado!');
+                setTimeout(function() {
+                    window.location.href = 'index.php?page=index';
+                });   
+            </script>";
+    }
+
+    if (isset($_REQUEST['submit_btn'])) {
+        $tipo = strtolower($_REQUEST['createtipo']);
+        $nome = strtolower($_REQUEST['createname']);
+        if (empty($tipo) || empty($nome)) {
+            echo "<script> alert('Existem campos vazios!') </script>";
+        } else {
+            try {
+                createProduct($conn, $tipo, $nome);
+            } catch (PDOException $e) {
+                throw new PDOException($e->getMessage());
             }
         }
+    }
     ?>
 </body>
+
 </html>
--- read.php
+++ read.php
@@ -1,100 +1,104 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
 </head>
+
 <body>
     <h2>Consultar Produto</h2>
     <form action="?page=read" method="POST">
         <div class="mb-3">
             <label for="TipoConsulta" class="form-label">Tipo</label>
-            <input type="text" class="form-control" id="TipoConsulta" name="readtipo">
+            <input type="text" class="form-control border-primary" id="TipoConsulta" name="readtipo">
         </div>
         <div class="mb-3">
             <label for="NomeConsulta" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="NomeConsulta" name="readnome">
+            <input type="text" class="form-control border-primary" id="NomeConsulta" name="readnome">
         </div>
         <button type="submit" class="btn btn-primary" name="btn_read">Consultar</button>
     </form>
     <style>
-    .table-container {
-        margin-left: 200px;
-    }
+        .table-container {
+            margin-left: 200px;
+        }
     </style>
     <?php
-    //se o botão for pressionado, a variável está setada
-        if(isset($_REQUEST["btn_read"])) {
-            //passa os valores dos inputs para lowercase
-            $rtipo = strtolower($_REQUEST["readtipo"]);
-            $rnome = strtolower($_REQUEST["readnome"]);
-            
-            function SearchByType($rtipo, $conn){
-                $query = "SELECT prod_nome, prod_cod, COUNT(*) AS quant FROM produto WHERE prod_tipo = :tipo GROUP BY prod_nome, prod_cod";
-                $sql = $conn ->prepare($query);
-                //bind de :nome como a variável $rtipo
-                $sql -> bindParam(':tipo', $rtipo, PDO::PARAM_STR);
-                $sql -> execute();
-                //fetch por nome de coluna
-                $data = $sql -> fetchAll(PDO::FETCH_ASSOC);
-                if(count($data) == 0){
-                    echo "Não foram encontrados resultados!";
-                } else {
-                    //exibe os dados em tabela
-                    echo "<div class='container text-center table-container'>";
+    if (isset($_REQUEST["btn_read"])) {
+
+        $rtipo = strtolower($_REQUEST["readtipo"]);
+        $rnome = strtolower($_REQUEST["readnome"]);
+
+        //pesquisa por tipo
+        function SearchByType($rtipo, $conn)
+        {
+            $query = "SELECT prod_nome, prod_cod, COUNT(*) AS quant FROM produto WHERE prod_tipo = :tipo GROUP BY prod_nome, prod_cod";
+            $sql = $conn->prepare($query);
+            $sql->bindParam(':tipo', $rtipo, PDO::PARAM_STR);
+            $sql->execute();
+            $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+            if (count($data) == 0) {
+                echo "Não foram encontrados resultados!";
+            } else {
+                //exibe os dados em tabela
+                echo "<div class='container text-center table-container'>";
+                echo "<div class='row'>";
+                echo "<div class='col'><h3><b>Nome</b></h3></div>";
+                echo "<div class='col'><h3><b>Quantidade</b></h3></div>";
+                echo "<div class='col'><h3><b>ID</b></h3></div>";
+                echo "</div>";
+                foreach ($data as $row) {
+                    echo "<div class='container text-center'>";
                     echo "<div class='row'>";
-                    echo "<div class='col'><h3><b>Nome</b></h3></div>";
-                    echo "<div class='col'><h3><b>Quantidade</b></h3></div>";
-                    echo "<div class='col'><h3><b>ID</b></h3></div>";
+                    echo "<div class='col'>" . "<h3>" . $row['prod_nome'] . "</h3>" . "</div>";
+                    echo "<div class='col'>" . "<h3>" . $row['quant'] . "</h3>" . "</div>";
+                    echo "<div class='col'>" . "<h3>" . $row['prod_cod'] . "</h3>" . "</div>";
                     echo "</div>";
-                    foreach($data as $row) {
-                        echo "<div class='container text-center'>";
-                        echo "<div class='row'>";
-                        echo "<div class='col'>" . "<h3>" . $row['prod_nome'] . "</h3>" . "</div>";
-                        echo "<div class='col'>" . "<h3>" . $row['quant'] . "</h3>" ."</div>";
-                        echo "<div class='col'>" . "<h3>" . $row['prod_cod'] . "</h3>" ."</div>";
-                        echo "</div>";
-                        echo "</div>";
-                    }
+                    echo "</div>";
                 }
             }
+        }
 
-            function SearchByName($rnome,$conn){
-                $query = "SELECT prod_tipo, prod_cod, COUNT(*) AS quant FROM produto WHERE prod_nome = :nome GROUP BY prod_tipo, prod_cod";
-                    $sql = $conn ->prepare($query);
-                    $sql -> bindParam(':nome', $rnome, PDO::PARAM_STR);
-                    $sql -> execute();
-                    $data = $sql -> fetchAll(PDO::FETCH_ASSOC);
-                    if(count($data) == 0) {
-                        echo "Não foram encontrados resultados!";
-                    } else {
-                        echo "<div class='container text-center table-container'>";
-                        echo "<div class='row'>";
-                        echo "<div class='col'><h3><b>Tipo</b></h3></div>";
-                        echo "<div class='col'><h3><b>Quantidade</b></h3></div>";
-                        echo "<div class='col'><h3><b>ID</b></h3></div>";
-                        echo "</div>";
-                        foreach($data as $row) {
-                            echo "<div class='container text-center'>";
-                            echo "<div class='row'>";
-                            echo "<div class='col'>" . "<h3>".$row['prod_tipo']."<h3>" . "</div>";
-                            echo "<div class='col'>" . "<h3>".$row['quant']."<h3>" ."</div>";
-                            echo "<div class='col'>" . "<h3>".$row['prod_cod']."<h3>" ."</div>";
-                            echo "</div>";
-                            echo "</div>";
-                        }
-                    }
-            }
-
-            if(empty($rtipo) && empty($rnome)) {
-                echo "<script> alert('Campos vazios!') </script>";
+        // pesquisa por nome
+        function SearchByName($rnome, $conn)
+        {
+            $query = "SELECT prod_tipo, prod_cod, COUNT(*) AS quant FROM produto WHERE prod_nome = :nome GROUP BY prod_tipo, prod_cod";
+            $sql = $conn->prepare($query);
+            $sql->bindParam(':nome', $rnome, PDO::PARAM_STR);
+            $sql->execute();
+            $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+            if (count($data) == 0) {
+                echo "Não foram encontrados resultados!";
             } else {
-                if(!empty($rtipo)) {
-                    SearchByType($rtipo, $conn);
-                }   
-                if(!empty($rnome)) {
-                    SearchByName($rnome, $conn);
-                }            
+                echo "<div class='container text-center table-container'>";
+                echo "<div class='row'>";
+                echo "<div class='col'><h3><b>Tipo</b></h3></div>";
+                echo "<div class='col'><h3><b>Quantidade</b></h3></div>";
+                echo "<div class='col'><h3><b>ID</b></h3></div>";
+                echo "</div>";
+                foreach ($data as $row) {
+                    echo "<div class='container text-center'>";
+                    echo "<div class='row'>";
+                    echo "<div class='col'>" . "<h3>" . $row['prod_tipo'] . "<h3>" . "</div>";
+                    echo "<div class='col'>" . "<h3>" . $row['quant'] . "<h3>" . "</div>";
+                    echo "<div class='col'>" . "<h3>" . $row['prod_cod'] . "<h3>" . "</div>";
+                    echo "</div>";
+                    echo "</div>";
+                }
             }
         }
-    ?>   
+
+        if (empty($rtipo) && empty($rnome)) {
+            echo "<script> alert('Campos vazios!') </script>";
+        } else {
+            if (!empty($rtipo)) {
+                SearchByType($rtipo, $conn);
+            }
+            if (!empty($rnome)) {
+                SearchByName($rnome, $conn);
+            }
+        }
+    }
+    ?>
 </body>
+
 </html>
