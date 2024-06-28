6f765eb90ec6bea2af34de91b7478abcc6a50137
gabrielzj <gabrielzuinj@gmail.com>
2024-06-27 22:43:42 -0300

    minor changes

--- dbconfig.php
+++ dbconfig.php
@@ -1,19 +1,18 @@
 <?php
-    // include 1 vez, poderia usar require_once
-    include_once __DIR__ . '/vendor/autoload.php';
-    // variaveis .env n sobrescrevem outras env variaveis
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->safeLoad();
-    
-    $db_host = $_ENV['DATABASE_HOST'];
-    $db_user = $_ENV['DATABASE_USER'];
-    $db_pass = $_ENV['DATABASE_PASSWORD'];
-    $db_name = $_ENV['DATABASE_NAME'];
+include_once __DIR__ . '/vendor/autoload.php';
 
-    // conexao com o banco usando PDO
-    try {
-        $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, $conn::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        throw new PDOException($e);
-    }
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+$db_host = $_ENV['DATABASE_HOST'];
+$db_user = $_ENV['DATABASE_USER'];
+$db_pass = $_ENV['DATABASE_PASSWORD'];
+$db_name = $_ENV['DATABASE_NAME'];
+
+// conexao com o banco usando PDO
+try {
+    $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    throw new PDOException($e);
+}
